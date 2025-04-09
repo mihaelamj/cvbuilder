107885194cda883b890ee366d0299b48b63c2b87
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-04-10 01:38:06 +0200

    fixed tests

--- Tests/CVBuilderTests/CVBuilderTests.swift
+++ Tests/CVBuilderTests/CVBuilderTests.swift
@@ -6,25 +6,9 @@ import Testing
     let cv = CV.createExampleCV()
     print("Created")
     #expect(cv.title == "Senior Mobile Developer")
-}
-
-@Test func testCreatingMihaelasCV() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    let mmjCV = CV.createForMihaela()
-    print("Created")
-    #expect(mmjCV.name == "Mihaela Mihaljević Jakić")
     
-    let stringRenderer = StringCVRenderer()
-    let stringOutput = stringRenderer.render(cv: mmjCV)
-    print("Rendered String Output:\n\n\(stringOutput)")
-
-    let consoleRenderer = ConsoleCVRenderer()
-    consoleRenderer.printToConsole(cv: mmjCV)
-
-    #expect(mmjCV.name.isEmpty == false)
-
-    if let markdownFile = CV.convertTMarkdownAndSave(mmjCV) {
+    if let markdownFile = CV.convertTMarkdownAndSave(cv) {
         print("Markdown saved to: \(markdownFile)")
     }
-    
 }
+
