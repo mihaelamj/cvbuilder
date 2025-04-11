fab2eab022f241fd285e983e4b58ce059eadcedf
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-04-12 01:21:54 +0200

    Add platform requirements for macOS 13 and iOS 16

--- Package.swift
+++ Package.swift
@@ -5,15 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "CVBuilder",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16)  
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CVBuilder",
             targets: ["CVBuilder"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CVBuilder"),
         .testTarget(
